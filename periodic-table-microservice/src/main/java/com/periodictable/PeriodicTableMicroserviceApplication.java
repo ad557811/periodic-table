88640342d7f2c471310cd54504884da2c3ece6b1
ad5578711 <45101859+ad557811@users.noreply.github.com>
2025-12-23 11:26:16 +0100

    added docs, cleaned cors filter

--- periodic-table-microservice/src/main/java/com/periodictable/PeriodicTableMicroserviceApplication.java
+++ periodic-table-microservice/src/main/java/com/periodictable/PeriodicTableMicroserviceApplication.java
@@ -1,17 +1,30 @@
 package com.periodictable;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.reactive.config.CorsRegistry;
 
-import java.util.Arrays;
+import java.util.List;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Periodic table API",
+        version = "1.0",
+        description = "API for listing elements from the Periodic table"
+    )
+)
 @SpringBootApplication
 public class PeriodicTableMicroserviceApplication {
+
+  @Value("${cors.frontend-address}")
+  private String frontendAddress;
 
   public static void main(String[] args) {
     SpringApplication.run(PeriodicTableMicroserviceApplication.class, args);
@@ -20,9 +33,9 @@ public class PeriodicTableMicroserviceApplication {
   @Bean
   CorsWebFilter corsWebFilter() {
     CorsConfiguration corsConfig = new CorsConfiguration();
-    corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+    corsConfig.setAllowedOrigins(List.of(frontendAddress));
     corsConfig.setMaxAge(8000L);
-    corsConfig.addAllowedMethod(CorsConfiguration.ALL);
+    corsConfig.addAllowedMethod(HttpMethod.GET);
     corsConfig.addAllowedHeader(CorsConfiguration.ALL);
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
--- periodic-table-microservice/src/main/java/com/periodictable/controller/PeriodicTableElementController.java
+++ periodic-table-microservice/src/main/java/com/periodictable/controller/PeriodicTableElementController.java
@@ -2,12 +2,22 @@ package com.periodictable.controller;
 
 import com.periodictable.entity.Element;
 import com.periodictable.service.PeriodicTableElementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Tag(name = "Periodic Table Controller", description = "Controller for returning elements of the periodic table.")
 @RestController
 @RequestMapping("/element")
 public class PeriodicTableElementController {
@@ -18,10 +28,15 @@ public class PeriodicTableElementController {
     this.service = service;
   }
 
+  @Operation(summary = "Returns a partial list of elements.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Element.class))})
+  })
   @GetMapping
-  public Flux<Element> getElements(@RequestParam(name = "query", required = false) String query,
-                                   @RequestParam(name = "max", required = false, defaultValue = "10") int pageSize,
-                                   @RequestParam(name = "page", required = false, defaultValue = "0") int pageIndex) {
+  public Flux<Element> getElements(
+      @RequestParam(name = "query", required = false) @Parameter(name = "query", description = "Filter for additional keywords", example = "gas") String query,
+      @RequestParam(name = "max", required = false, defaultValue = "10") @Parameter(name = "max", description = "Max number of results per page. Default is 10") int pageSize,
+      @RequestParam(name = "page", required = false, defaultValue = "0") @Parameter(name = "page", description = "Which page to return. Default is 0 (first page)") int pageIndex) {
     return service.getElements(query, pageIndex, pageSize);
   }
 
